310a34f5282c8a0b222ca325349067e6e447c038
Oleksandr2891 <allavrenko@gmail.com>
2021-09-27 21:22:41 +0300

    task done

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -12,11 +12,12 @@ import {
 } from "redux-persist";
 import thunk from "redux-thunk";
 import storage from "redux-persist/lib/storage";
-import authReducer, { IAuthInitialState } from "./auth/auth-slice";
+import authReducer from "./auth/auth-slice";
 import { error } from "./error/error-reducer";
 import projects from "./projects/projects-slice";
 import sprints from "./sprints/sprints-slice";
 import tasks from "./task/task-slice";
+import { IAuthInitialState } from '../redux/interfaces/interfacesStore';
 
 const authPersistConfig = {
   key: "auth",
