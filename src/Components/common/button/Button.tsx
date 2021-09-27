3544b68094c68c35f9d0a38592ed0034f7b90879
PetiurAliona <alena.petyur@gmail.com>
2021-09-27 17:29:02 +0300

    +TS Button, SubmitButton

--- src/Components/common/button/Button.tsx
+++ src/Components/common/button/Button.tsx
@@ -1,11 +1,19 @@
 import { ButtonWrapper } from './ButtonStyled';
 
+
+interface Props {
+  onHandleClick: React.MouseEventHandler<HTMLButtonElement>,
+  icon: string,
+  classBtn: string,
+  background: string,
+}
+
 const Button = ({
   onHandleClick,
   icon = 'add',
   classBtn = 'add',
   background = '',
-}) => {
+}: Props) => {
   return (
     <ButtonWrapper>
       <button
