28d187d6b8d5aebfa47cde2267d959bc4d1a8310
TheDeveloperAlex <scorpion422@ukr.net>
2021-09-29 00:40:52 +0300

    Добавляет ts к taskList

--- src/Components/tasks/taskList/TaskList.tsx
+++ src/Components/tasks/taskList/TaskList.tsx
@@ -6,6 +6,7 @@ import { useDispatch } from "react-redux";
 import { getSprintsTasks } from "../../../redux/task/task-operations";
 import { useParams } from "react-router-dom";
 import taskSelectors from "../../../redux/task/task-selectors";
+import { taskInterface } from '../taskInterfaces/taskInterfaces'
 
 const TaskList = ({ filter, targetDate }) => {
   const tasks = useSelector(taskSelectors.getTasks);
@@ -17,7 +18,7 @@ const TaskList = ({ filter, targetDate }) => {
     dispatch(getSprintsTasks(id));
   }, [dispatch, id]);
   useEffect(() => {
-    const res = tasks.filter((task) => task.title?.includes(filter));
+    const res = tasks.filter((task: taskInterface) => task.title?.includes(filter));
     setFiltredTasks(res);
   }, [filter, tasks]);
 
@@ -25,7 +26,7 @@ const TaskList = ({ filter, targetDate }) => {
     <TasklistStyled>
       {filtredTasks.length === 0 &&
         filter.length === 0 &&
-        tasks.map((item) => (
+        tasks.map((item: taskInterface) => (
           <TaskListItem
             key={item._id ?? item.id}
             task={item}
@@ -33,7 +34,7 @@ const TaskList = ({ filter, targetDate }) => {
           />
         ))}
       {filtredTasks.length > 0 &&
-        filtredTasks.map((item) => (
+        filtredTasks.map((item: taskInterface) => (
           <TaskListItem
             key={item._id ?? item.id}
             task={item}
