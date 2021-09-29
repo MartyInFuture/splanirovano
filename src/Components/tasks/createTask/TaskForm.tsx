059fc6132d3cdd37eae412b12dca7da1566cd0ce
PetiurAliona <alena.petyur@gmail.com>
2021-09-29 11:36:12 +0300

    + попередження

--- src/Components/tasks/createTask/TaskForm.tsx
+++ src/Components/tasks/createTask/TaskForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import { useDispatch } from "react-redux";
 import { addTask } from "../../../redux/task/task-operations";
 
@@ -6,6 +6,7 @@ import SubmitButton from "../../common/submitButton/SubmitButton";
 import { WrapperForm } from "./TaskFormStyled";
 import { useParams } from "react-router";
 import { toast } from "react-toastify";
+
 
 interface ICloseModalProps {
   setCloseModal: (active: boolean) => void;
@@ -15,18 +16,29 @@ interface IId {
   id: string;
 }
 
+type THours = number;
+
 const TaskForm = ({ setCloseModal } : ICloseModalProps) => {
   const [title, setTitle] = useState("");
-  const [hoursPlanned, setHoursPlanned] = useState("");
-  const { id } = useParams<IId>();
+  const [hoursPlanned, setHoursPlanned] = useState<THours>(0);
+  const { id } = useParams<IId>();  
 
   const handleChangeTitle = (evt: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(evt.currentTarget.value);
   };
 
+  useEffect(() => {
+  
+  if (hoursPlanned > 8) {
+    toast.warning(
+      "Максимальний час на виконання однієї задачі - 8 год на день. Не перепрацьовуйте!"
+    );
+  }
+}, [hoursPlanned])
+
 
   const handleHoursPlanned = (evt: React.ChangeEvent<HTMLInputElement>) => {
-    setHoursPlanned(evt.currentTarget.value);  
+    setHoursPlanned(Number(evt.currentTarget.value));  
   };
 
   const dispatch = useDispatch();
@@ -34,8 +46,7 @@ const TaskForm = ({ setCloseModal } : ICloseModalProps) => {
   const onHandleSubmit = (evt:React.FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
     dispatch(addTask({ sprintId: id, task: { title, hoursPlanned } }));
-    setTitle("");
-    setHoursPlanned("");
+    setTitle("");    
     setCloseModal(false);
   };
 
@@ -63,6 +74,7 @@ const TaskForm = ({ setCloseModal } : ICloseModalProps) => {
             placeholder="Заплановано годин"
             required
             onChange={handleHoursPlanned}
+            min="0"
           />
         </label>
         <div className="submitWrapper">
